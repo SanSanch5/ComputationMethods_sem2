ae984636d743a9e16e9f81268d28378e5e222335
Alexander Pakhomov <a.a.pakhomoff@gmail.com>
2017-04-05 00:17:48 +0300

    fix a little

--- lab1/bitwise_search.m
+++ lab1/bitwise_search.m
@@ -1,12 +1,14 @@
-function x = bitwise_search(a, b, eps)
+function [x, N] = bitwise_search(a, b, eps)
 delta = (b-a)/4;
 x0 = a;
 f0 = f(a);
+N = 1;
 x = x0;
 
-while abs(delta) > eps
+while true
     x1 = x0 + delta;
     f1 = f(x1);
+    N = N+1;
     x = [x; x1];
     
     if f0 > f1
@@ -15,6 +17,9 @@ while abs(delta) > eps
         if x0 < b && x0 > a
             continue;
         end
+    end
+    if abs(delta) <= eps
+        break;
     end
     x0 = x1;
     f0 = f1;
--- lab1/lab1.m
+++ lab1/lab1.m
@@ -1,3 +1,5 @@
+clc;
+
 draw_solution_array = true;
 
 x = -1:1/100:0;
@@ -5,19 +7,25 @@ y = arrayfun(@f, x);
 
 eps_array = [10^-2, 10^-4, 10^-6];
 for eps = eps_array
-    x_solution_array = bitwise_search(-1, 0, eps);
-    [rc, cc] = size(x_solution_array);
+    [x_solution_array, N] = bitwise_search(-1, 0, eps);
     x_min = x_solution_array(end);
-    if ~draw_solution_array
-        x_solution_array = [];
-    end
-    y_solution_array = arrayfun(@f, x_solution_array);
-
     fprintf('Значение eps: %f\n', eps);
     fprintf('Значение x*: %.8f\n', x_min);
     fprintf('Значение функции f(x*): %.8f\n', f(x_min));
-    fprintf('Количество итераций: %d\n\n', cc);
+    fprintf('Количество итераций: %d\n', N);
 
+    if ~draw_solution_array
+        x_solution_array = [];
+    else
+        fprintf('xi,\tf(xi)\t:\n');
+    end
+    y_solution_array = arrayfun(@f, x_solution_array);
+
+    for x_i = x_solution_array
+        fprintf('%f\t%f\t\n', x_i, f(x_i));
+    end
+    fprintf('\n')
+    
     figure('Name', strcat('Точность: ', num2str(eps)),'NumberTitle', 'off');
     plot(x, y, '-', x_solution_array, y_solution_array, 'b*',... 
         x_min, f(x_min), 'ro') 
