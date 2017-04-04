3980badc0d8f1442e6f6d7b77870b9d3e3f5f985
Alexander Pakhomov <a.a.pakhomoff@gmail.com>
2017-04-05 00:47:58 +0300

    shit works

--- lab2/golden_section_search.m
+++ lab2/golden_section_search.m
@@ -1,5 +1,6 @@
-function x = golden_section_search(a, b, eps)
+function [segments, x_min, N] = golden_section_search(a, b, eps)
 
+segments = [a, b];
 tau = (sqrt(5)-1)/2;
 l = b-a;
 
@@ -7,11 +8,30 @@ x1 = b - tau*l;
 x2 = a + tau*l;
 f1 = f(x1);
 f2 = f(x2);
+N = 2;
 
 while l > 2*eps
-    
+    if f1 <= f2
+        b = x2;
+        l = b-a;
+        x2 = x1;
+        f2 = f1;
+        x1 = b - tau*l;
+        f1 = f(x1);
+        N = N+1;
+    else
+        a = x1;
+        l = b-a;
+        x1 = x2;
+        f1 = f2;
+        x2 = a + tau*l;
+        f2 = f(x2);
+        N = N+1;
+    end
+        segment = [a, b];
+        segments = [segments; segment];
 end
 
-
+x_min = (a + b) / 2;
 
 end
--- lab2/lab2.m
+++ lab2/lab2.m
@@ -1,3 +1,5 @@
+clc;
+
 draw_solution_array = true;
 
 x = -1:1/100:0;
@@ -5,20 +7,25 @@ y = arrayfun(@f, x);
 
 eps_array = [10^-2, 10^-4, 10^-6];
 for eps = eps_array
-    x_solution_array = bitwise_search(-1, 0, eps);
-    [rc, cc] = size(x_solution_array);
-    x_min = x_solution_array(end);
-    if ~draw_solution_array
-        x_solution_array = [];
-    end
-    y_solution_array = arrayfun(@f, x_solution_array);
+    [segments, x_min, N] = golden_section_search(-1, 0, eps);
 
     fprintf('Значение eps: %f\n', eps);
     fprintf('Значение x*: %.8f\n', x_min);
     fprintf('Значение функции f(x*): %.8f\n', f(x_min));
-    fprintf('Количество итераций: %d\n\n', cc);
+    fprintf('Количество итераций: %d\n\n', N);
+    
+    if ~draw_solution_array
+        segments = [];
+    else
+        fprintf('[\tai,\tbi\t]:\n');
+    end
+    
+    [n, m] = size(segments);
+    for row = 1 : n
+        fprintf('[\t%f\t,\t%f\t]\n', segments(row,1), segments(row,2));
+    end
+    fprintf('\n')
 
     figure('Name', strcat('Точность: ', num2str(eps)),'NumberTitle', 'off');
-    plot(x, y, '-', x_solution_array, y_solution_array, 'b*',... 
-        x_min, f(x_min), 'ro') 
+    plot(x, y, '-', x_min, f(x_min), 'ro') 
 end
